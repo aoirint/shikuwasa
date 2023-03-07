7f8e65ba47fd7757478be425152a0613ed71fbda
aoirint <aoirint@gmail.com>
2023-03-08 05:22:34 +0900

    fix test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shikuwasa/main.dart';
+import 'package:shikuwasa/flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
